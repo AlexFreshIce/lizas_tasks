f757dfde08e47db6933372e93e0cdfaf3074972f
AlexFreshIce <alexfreshice@yandex.ru>
2021-10-11 02:52:53 +0300

    task_24

---
+++ task_24.js
@@ -0,0 +1,94 @@
+// http://www.codewars.com/kata/calculating-with-functions
+function zero(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("0" + n));
+  } else {
+    return "0";
+  }
+}
+function one(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("1" + n));
+  } else {
+    return "1";
+  }
+}
+function two(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("2" + n));
+  } else {
+    return "2";
+  }
+}
+function three(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("3" + n));
+  } else {
+    return "3";
+  }
+}
+function four(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("4" + n));
+  } else {
+    return "4";
+  }
+}
+function five(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("5" + n));
+  } else {
+    return "5";
+  }
+}
+function six(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("6" + n));
+  } else {
+    return "6";
+  }
+}
+function seven(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("7" + n));
+  } else {
+    return "7";
+  }
+}
+function eight(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("8" + n));
+  } else {
+    return "8";
+  }
+}
+function nine(n) {
+  if (n) {
+    console.log(n);
+    return Math.floor(eval("9" + n));
+  } else {
+    return "9";
+  }
+}
+
+function plus(n) {
+  return "+" + n;
+}
+function minus(n) {
+  return "-" + n;
+}
+function times(n) {
+  return "*" + n;
+}
+function dividedBy(n) {
+  return "/" + n;
+}
